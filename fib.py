c6961bb8b12911361cca1042f6fb0b4776bcb392
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-18 22:10:30 -0700

    fib

--- fib.py
+++ fib.py
@@ -11,6 +11,6 @@ def fib(n):
 
 inp = input('Enter Fahrenheit Temperature: ')
 try:
-fahr = float(inp)
-cel = (fahr - 32.0) * 5.0 / 9.0
-print(cel)
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
